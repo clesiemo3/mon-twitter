b723a87e0c8cb78a62d2c9fb9dbbdab1e1bbaa2f
clesiemo3 <clesiemo3@gmail.com>
2016-08-28 21:57:24 -0500

    sql for creating table

---
+++ create_table.sql
@@ -0,0 +1,30 @@
+-- Table: tweets
+
+-- DROP TABLE tweets;
+
+CREATE TABLE tweets
+(
+  id character varying NOT NULL,
+  username character varying,
+  name character varying,
+  location character varying,
+  url character varying,
+  followers_count integer,
+  friends_count integer,
+  listed_count integer,
+  favourites_count integer,
+  statuses_count integer,
+  user_lang character varying,
+  favorited boolean,
+  retweeted boolean,
+  tweet_lang character varying,
+  timestamp_ms character varying,
+  create_dt timestamp with time zone NOT NULL DEFAULT now(),
+  inactive_dt timestamp with time zone DEFAULT '9999-09-09 00:00:00-05'::timestamp with time zone,
+  CONSTRAINT tweets_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE tweets
+  OWNER TO twpy;
